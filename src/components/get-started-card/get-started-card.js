0893f399721dac00b8cac99df2616aad5c26280a
ravisince2k <pabhinav400@gmail.com>
2023-07-29 09:58:11 +0530

    seperate get started card into component

---
+++ src/components/get-started-card/get-started-card.js
@@ -0,0 +1,21 @@
+import CustomButton from '../custom-button/custom-button';
+import styles from './get-started-card.module.scss';
+function GetStartedCard() {
+  return (
+    <div className={styles.askDemo}>
+      <div className={styles.group}>
+        <h3 className={styles.title}>Schedule a live demo</h3>
+        <p className={styles.description}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempos Lorem ipsum dolor sit amet, consectetur adipiscing
+          elit, sed do eiusmod tempos
+        </p>
+        <CustomButton gradient>Get started</CustomButton>
+      </div>
+      <img className={styles.image} src="/assets/images/bag.png" alt="" />
+      <img className={styles.bgBlur} src="/assets/images/blur-bg.png" alt="" />
+    </div>
+  );
+}
+
+export default GetStartedCard;
