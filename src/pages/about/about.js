930610a223bbf59e217c211042c0a0fc0757b392
ravisince2k <pabhinav400@gmail.com>
2023-08-03 12:47:12 +0530

    fix about us description

--- src/pages/about/about.js
+++ src/pages/about/about.js
@@ -5,19 +5,24 @@ import TeamGroup from 'src/components/team-group/team-group';
 const about = {
   image: '/assets/images/services.png',
   title: 'About Us',
-  subtitle:
+  description:
     'Since our inception, we have been pioneers in applying AI to address real-world challenges. Our unwavering dedication to providing scalable solutions has led us to deliver substantial value to our clients, leaving a lasting impact in diverse industries such as Finance, Sales, Real Estate, Sports, Marketing, and more. Along this journey, we have accumulated invaluable experience through both failures and successes. XYZ represents the culmination of our efforts to democratize AI adoption, making it accessible to all. As we enter the era of generative AI, we are committed to ensuring that no one is left behind. Our goal is to harness the power of AI for the collective advancement of society, reaping its benefits together.'
 };
 
 function AboutPage() {
   return (
     <div className={styles.aboutPage}>
-      <Head
+      <div className={styles.head}>
+        <h1 className={styles.title}>{about?.title}</h1>
+        <div className={styles.description}>{about?.description}</div>
+      </div>
+      {/* <Head
         noBtns
         gradientTitle={about?.title}
         subtitle={about?.subtitle}
+        wide
         // image={about.image}
-      />
+      /> */}
       {/* <Journery /> */}
       <TeamGroup />
     </div>
